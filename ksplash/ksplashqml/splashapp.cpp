5b4079dc77c828d0f5ec5734c66396d6d8d7c505
lingmo-dream <2422586862@qq.com>
2025-02-11 23:47:31 +0800

    update

--- ksplash/ksplashqml/splashapp.cpp
+++ ksplash/ksplashqml/splashapp.cpp
@@ -114,7 +114,7 @@ void SplashApp::setStage(const QString &stage)
 void SplashApp::setStage(int stage)
 {
     m_stage = stage;
-    if (m_stage == 5) {
+    if (m_stage == 7) {
         QGuiApplication::exit(EXIT_SUCCESS);
     }
     foreach (SplashWindow *w, m_windows) {
